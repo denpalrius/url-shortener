9ed4ec1f7e038c34cf3193d62a4fff8687e00090
Dennis Riungu <denpalrius@gmail.com>
2021-03-05 00:44:58 +0300

    Add swagger and helmet

---
+++ src/configs/swagger.ts
@@ -0,0 +1,15 @@
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+export async function configureSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('My URL shortener')
+    .setDescription(
+      `This is the OpenAPI documentation for the "URL Shortener API"`,
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { configureSwagger } from './configs/swagger';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Some security
+  app.enableCors();
+  app.use(helmet());
+
+  await configureSwagger(app);
+
   await app.listen(3000);
 }
+
 bootstrap();
